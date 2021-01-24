cdce2ee0cc93d49340b974d69eff41893b71799d
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-01-24 15:58:10 +0530

    react jsx demo

---
+++ jsx-demo/src/index.js
@@ -0,0 +1,18 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a react component
+const App = () => {
+    return (
+        <div>
+            <h1>Hii There!!!</h1>
+        </div>
+    )
+}
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
