7044a8538d6ee9821cf8890e02464bf66f5fe5b3
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-04-27 19:14:52 +0530

    jsx-demo basics

--- jsx-demo/src/index.js
+++ jsx-demo/src/index.js
@@ -4,9 +4,13 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
+    const buttonText = 'Click Me!';
     return (
-        <div>
+        <div className="mainContainer">
             <h1>Hii There!!!</h1>
+            <label htmlFor="name">Enter your Name: </label>
+            <input id="name" type="text"/>
+            <button type="submit" style={{backgroundColor: "blue", color: "white"}}>{buttonText}</button>
         </div>
     )
 }
