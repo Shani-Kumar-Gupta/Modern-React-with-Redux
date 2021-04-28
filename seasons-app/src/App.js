316e685dce31230b83bfc5907bea4b365214c76c
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-04-28 20:28:14 +0530

    class based comoponent and initialized state in constructor

--- seasons-app/src/App.js
+++ seasons-app/src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const App = () => {
-    return(
-        <div>Hii There!</div>
-    )
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (err) => console.log(err)
+//     );
+
+//     return(
+//         <div>Hii There!</div>
+//     )
+// }
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            lat: null,
+        }
+    }
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+        return(
+            <div>Latitude: </div>
+        )
+    }
 }
 
 export default App;
