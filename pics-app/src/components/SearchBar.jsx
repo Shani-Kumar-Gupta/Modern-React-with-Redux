9a972be946b36b861b700c549844018d9a609734
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-05-02 14:44:01 +0530

    added input event handler: onChange and onClick

--- pics-app/src/components/SearchBar.jsx
+++ pics-app/src/components/SearchBar.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
+    onInputClick() {
+        console.log('Input is clicked');
+    }
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input 
+                            type="text"
+                            onClick={this.onInputClick} 
+                            onChange={this.onInputChange} />
                     </div>
                 </form>
             </div>
