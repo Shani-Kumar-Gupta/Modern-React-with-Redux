f2763575ff0a977b2450d8b54ed4360e24468ed1
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-05-08 21:16:36 +0530

    using Ref's for DOM manipulation

--- pics-app/src/components/ImagesList.jsx
+++ pics-app/src/components/ImagesList.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
+import '../css/ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImagesList = (props) => {
     const { images } = props;
-    const image = images.map(({id, urls, description}) => {
-        return <img key={id} src={urls.regular} alt={description} />
+    const image = images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
     })
 
     return (
-        <div>
+        <div className="image-list">
             {image}
         </div>
     )
