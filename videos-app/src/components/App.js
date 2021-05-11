d9ec04aef93e13d3c62cb2b1dde77917a53585d2
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-05-11 23:31:56 +0530

    done with video app

--- videos-app/src/components/App.js
+++ videos-app/src/components/App.js
@@ -11,6 +11,10 @@ class App extends React.Component {
         selectedVideo: null
     };
 
+    componentDidMount() {
+        this.onTermSubmit('hello world');
+    }
+
     onTermSubmit = async (term) => {
     
         const response = await youtube.get('/search', {
@@ -20,7 +24,8 @@ class App extends React.Component {
         });
 
         this.setState({
-            videos: response.data.items
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
         })
     }
 
@@ -34,8 +39,16 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
