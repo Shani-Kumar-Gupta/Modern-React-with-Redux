79cc1d1f44cbc7eea9c1a974ae0656dfabbc2093
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-05-14 14:58:08 +0530

    created Accordion and Search widget component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion';
+import items from './helpers/index';
+import Search from './components/Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="ui container">
+            <br />
+            <Search />
+            <br />
+            <Accordion items={items} />
+        </div>
+    )
 }
 
 export default App;
---
+++ widgets/src/helpers/index.js
@@ -0,0 +1,19 @@
+const items = [
+    {
+        _id: 1,
+        title: 'What is React?',
+        content: 'React is an open-source, front end, JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.'
+    },
+    {
+        _id: 2,
+        title: 'Why use React?',
+        content: 'It\'s used for handling the view layer for web and mobile apps. React also allows us to create reusable UI components. React allows developers to create large web applications that can change data, without reloading the page. The main purpose of React is to be fast, scalable, and simple.'
+    },
+    {
+        _id: 3,
+        title: 'How do you use React?',
+        content: 'Use React by creating Components.'
+    },
+]
+
+export default items;
