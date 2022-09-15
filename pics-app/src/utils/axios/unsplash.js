65da0f104280d355f87987d717605ca47cf39fff
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2022-09-15 23:25:50 +0530

    initial commit

--- pics-app/src/utils/axios/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com', 
-    headers: {
-        Authorization: 'Client-ID PnGDzIXbzPvJS-LuM8XRwVr1YwPVgTb1O4Oj-stzkuU'
-    }
-});
