8d6d897094e81f812045fc4073c583122d4c5b40
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2022-11-27 18:57:53 +0530

    React Component created

---
+++ jsx/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Equivalent JavaScript after transpiles from Babel
+// function App() {
+//   return React.createElement(
+//     'div',
+//     null,
+//     React.createElement('h1', null, 'Hello world'),
+//     React.createElement('p', null, 'Shani Kumar Gupta')
+//   );
+// }
+
+function App() {
+  return <h1>Hello React!!!</h1>;
+}
+
+export default App;
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,6 +1,7 @@
 // 1) Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App.js';
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById('root');
@@ -9,9 +10,9 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
-function App() {
-  return <h1>Hi There!</h1>;
-}
+// function App() {
+//   return <h1>Hi There!</h1>;
+// }
 
 // 5) Show the component on the screen
 root.render(<App />);
