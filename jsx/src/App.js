a288ac416903857afd8943bcb464d406332cd0f1
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2022-12-01 22:45:23 +0530

    Module System: Default and Named Export

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,3 +1,4 @@
+// Module System: Import and Export
 import React from 'react';
 
 // Equivalent JavaScript after transpiles from Babel
@@ -10,21 +11,36 @@ import React from 'react';
 //   );
 // }
 
-function App() {
-  let message = 'Bye There!!';
-  const name = 'Shani Kumar Gupta';
-  let age = 25;
-  if (Math.random() > 0.5) {
-    message = 'Hii There!!';
-  }
+// function App() {
+//   let message = 'Bye There!!';
+//   const name = 'Shani Kumar Gupta';
+//   let age = 25;
+//   if (Math.random() > 0.5) {
+//     message = 'Hii There!!';
+//   }
+//   return (
+//     <>
+//       <h1>{message}</h1>
+//       <p>Time: {new Date().toLocaleDateString()}</p>
+//       <p>Time: {new Date().toLocaleTimeString()}</p>
+//       <p>Hi, My name is {name + ' and my age is ' + age}</p>
+//       <input maxLength={5} minLength={2} />
+//       {/* <meter /> */}
+//       <textarea autoFocus />
+//     </>
+//   );
+// }
+
+function Demo() {
   return (
     <>
-      <h1>{message}</h1>
-      <p>Time: {new Date().toLocaleDateString()}</p>
-      <p>Time: {new Date().toLocaleTimeString()}</p>
-      <p>Hi, My name is {name + ' and my age is ' + age}</p>
+      <h1>Hi! Demo App</h1>
     </>
   );
 }
 
-export default App;
+export const MyMessage = 'Hi! React';
+const myData = 5;
+
+export { myData }; // Named Export
+export default Demo; // Default Export
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,13 +1,14 @@
 // 1) Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
+import MyApp, { myData, MyMessage } from './App.js';
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById('root');
 
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
+console.log(myData, MyMessage);
 
 // 4) Create a component
 // function App() {
@@ -15,4 +16,4 @@ const root = ReactDOM.createRoot(el);
 // }
 
 // 5) Show the component on the screen
-root.render(<App />);
+root.render(<MyApp />);
